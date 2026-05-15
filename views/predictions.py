bb011e3ef55fa10f426e9a6623de4dd66b3401c7
RomiCocaPermont <romipermont@gmail.com>
2026-05-15 16:56:19 +1200

    removing error repeated side bar in predictionspage

--- views/predictions.py
+++ views/predictions.py
@@ -2,7 +2,6 @@ import streamlit as st
 from utils.sidebar import render_sidebar
 
 def show():
-    render_sidebar("predictions")
-
+    
     st.markdown("<div class='page-title'>Predictive Delays</div>", unsafe_allow_html=True)
     st.write("Prediction results will go here.")
